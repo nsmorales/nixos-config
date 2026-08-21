82fa8258d711c40e7881e7790c57baadaef3a3e4
%NAME% <nsmorales@users.noreply.github.com>
2026-08-21 09:28:46 -0400

    Fix greetd starting hyperland

--- templates/starter/hosts/nixos/default.nix
+++ templates/starter/hosts/nixos/default.nix
@@ -70,7 +70,8 @@ in
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'uwsm start hyprland-uwsm.desktop'";
+          # start-hyprland is Hyprland's session launcher (handles systemd env import)
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'start-hyprland'";
           user = "greeter";
         };
       };
