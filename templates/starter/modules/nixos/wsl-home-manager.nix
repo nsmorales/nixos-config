789716c3eb1c97d325663dd5a1c6cf46d81fbcae
%NAME% <%EMAIL%>
2026-09-25 13:18:00 +0000

    Attach to tmux by default on startup/opening shell.\

--- templates/starter/modules/nixos/wsl-home-manager.nix
+++ templates/starter/modules/nixos/wsl-home-manager.nix
@@ -9,6 +9,14 @@ in
   # Shared programs (zsh+omz+p10k, git, vim, ghostty, tmux, ssh)
   imports = [ ../shared/home-manager.nix ];
 
+  # Auto-attach to tmux when WSL launches a shell.
+  # Skips nested shells inside tmux and non-interactive shells.
+  programs.zsh.initExtra = ''
+    if [[ -z "$TMUX" && $- == *i* ]]; then
+      exec tmux new-session -A -s default
+    fi
+  '';
+
   home = {
     enableNixpkgsReleaseCheck = false;
     username = user;
